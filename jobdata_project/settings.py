49a43898ca34d8117ecaeee214e0cd64b3879237
Matías Senia <matiassenia9@gmail.com>
2024-07-04 00:23:33 -0300

    Actualización de configuración en settings.py

--- jobdata_project/settings.py
+++ jobdata_project/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +15,7 @@ SECRET_KEY = 'django-insecure-bclgmp4z!pk0j4-5+3x6_u*aaa=#fw*6@vmlwq&+nj(5nit6%i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'gastrosal.vercel.app/', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -65,10 +66,7 @@ WSGI_APPLICATION = 'jobdata_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
 }
 
 
@@ -107,7 +105,11 @@ USE_TZ = True # Habilita el uso de la zona horaria
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'jobdata/static/jobdata')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'jobdata/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
