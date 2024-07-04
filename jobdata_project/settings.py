2a48824954bd84d675504dd2445d98b89e8c113d
Matias Ezequiel Senia <79487757+matiassenia@users.noreply.github.com>
2024-07-04 19:10:44 -0300

    Update settings.py

--- jobdata_project/settings.py
+++ jobdata_project/settings.py
@@ -1,32 +1,30 @@
 import os
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
-import environ
 import dj_database_url
 
 # Define BASE_DIR before reading the .env file
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Initialize environment variables
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobdata_project.settings')
-
-# Configure WSGI application for serving static files
-
+# Load environment variables from .env file
+dotenv_path = os.path.join(BASE_DIR, '.env')
+if os.path.exists(dotenv_path):
+    with open(dotenv_path, 'r') as f:
+        for line in f:
+            if line.strip() and not line.startswith('#'):
+                key, value = line.strip().split('=', 1)
+                os.environ[key] = value
+    
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', default='django-insecure-bclgmp4z!pk0j4-5+3x6_u*aaa=#fw*6@vmlwq&+nj(5nit6%i')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-bclgmp4z!pk0j4-5+3x6_u*aaa=#fw*6@vmlwq&+nj(5nit6%i')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['.vercel.app', 'gastrosal.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+#['.vercel.app', 'gastrosal.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -131,3 +129,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # CSRF Trusted Origins
 CSRF_TRUSTED_ORIGINS = ['https://gastrosal.vercel.app']
+
