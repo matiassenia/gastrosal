4baa874f79f81f894b1acb7eac1efff7a7b06966
Matías Senia <matiassenia9@gmail.com>
2024-07-04 00:40:04 -0300

    update settings

--- jobdata_project/settings.py
+++ jobdata_project/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True # Habilita el uso de la zona horaria
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'jobdata/static/jobdata')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'jobdata/static'),
