c49e2e598c5bb690c284a40016eaf37b8329d9b7
Matías Senia <matiassenia9@gmail.com>
2024-07-05 13:51:45 -0300

    gunicorn

--- gastrosal/settings.py
+++ gastrosal/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
-#from django.core.wsgi import get_wsgi_application
 import dj_database_url
+from django.core.wsgi import get_wsgi_application
 
 # Define BASE_DIR before reading the .env file
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,21 +14,16 @@ if os.path.exists(dotenv_path):
             if line.strip() and not line.startswith('#'):
                 key, value = line.strip().split('=', 1)
                 os.environ[key] = value
-    
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-bclgmp4z!pk0j4-5+3x6_u*aaa=#fw*6@vmlwq&+nj(5nit6%i')
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-bclgmp4z!pk0j4-5+3x6_u*aaa=#fw*6@vmlwq&+nj(5nit6%i')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False) == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
-#['.vercel.app', 'gastrosal.vercel.app', '127.0.0.1', 'localhost']
-
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -36,14 +31,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',  # corregido: falta una coma
     'crispy_forms',
     'jobdata',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -73,21 +68,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'gastrosal.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-import dj_database_url
-
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:zjLfuqgrfljSaUbuJhDgNTDimSZuOWoZ@viaduct.proxy.rlwy.net:15018/railway',
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True,
     )
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -104,45 +93,35 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
-LANGUAGE_CODE = 'es'  # Establece el código de idioma para español
-TIME_ZONE = 'UTC'  # Configura la zona horaria según sea necesario
-USE_I18N = True  # Habilita la internacionalización
-USE_L10N = True  # Habilita la localización
-USE_TZ = True  # Habilita el uso de la zona horaria
+LANGUAGE_CODE = 'es'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'jobdata/static'),
 ]
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # CSRF Trusted Origins
-#CSRF_TRUSTED_ORIGINS = ['https://gastrosal.vercel.app']
-# CSRF Trusted Origins
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
-
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'https://gastrosal-production.up.railway.app').split(',')
 
 # Extra: Configuración del puerto
 PORT = os.environ.get('PORT', '8000')
 
-ALLOWED_HOSTS.append('127.0.0.1')  # Agrega localhost a los hosts permitidos
-ALLOWED_HOSTS.append('localhost')
+ALLOWED_HOSTS.extend(['127.0.0.1', 'localhost', 'gastrosal-production.up.railway.app'])
 
-# Configuración para vercel:
+# Configuración para Railway:
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gastrosal.settings')
 application = get_wsgi_application()
