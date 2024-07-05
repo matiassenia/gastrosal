d2fd163801560c2188b654958c5eb23905901b13
Matías Senia <matiassenia9@gmail.com>
2024-07-05 00:48:22 -0300

    update venv

--- gastrosal/settings.py
+++ gastrosal/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'jobdata_project.urls'
+ROOT_URLCONF = 'gastrosal.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'jobdata_project.wsgi.application'
+WSGI_APPLICATION = 'gastrosal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
--- gastrosal/wsgi.py
+++ gastrosal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for jobdata_project project.
+WSGI config for gastrosal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobdata_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gastrosal.settings')
 
 application = get_wsgi_application()
 
