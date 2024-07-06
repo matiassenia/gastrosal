57571b72f0462048594c4a71227c9d6b1f91629e
Matías Senia <matiassenia9@gmail.com>
2024-07-05 21:02:01 -0300

    up jobdata

--- jobdata/urls.py
+++ jobdata/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.upload_data, name='upload_data'),
     path('upload/', views.upload_data, name='upload_data'),
     path('data_list/', views.data_list, name='data_list'),
 ]
