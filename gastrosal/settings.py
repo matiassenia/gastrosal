e6ec0351ff954f28cd4e4f472f8cde681c5f24a6
Matías Senia <matiassenia9@gmail.com>
2024-07-07 01:39:51 -0300

    upd datab

--- gastrosal/settings.py
+++ gastrosal/settings.py
@@ -1,12 +1,14 @@
 import os
 from pathlib import Path
+import dj_database_url 
 from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
+
+load_dotenv()
 
 # Define BASE_DIR before reading the .env file
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-bclgmp4z!pk0j4-5+3x6_u*aaa=#fw*6@vmlwq&+nj(5nit6%i')
@@ -62,21 +64,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'gastrosal.wsgi.application'
 
 
-#DATABASES = {
-#    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-#}
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 # Database
-DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':'railway',
-        'USER':'postgres',
-        'PASSWORD':'TNnisYqbnGOtZUCAeyBVntwqclLDFoTX',
-        'HOST':'monorail.proxy.rlwy.net',
-        'PORT':'37944',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE':'django.db.backends.postgresql',
+#        'NAME':'railway',
+#        'USER':'postgres',
+#        'PASSWORD':'TNnisYqbnGOtZUCAeyBVntwqclLDFoTX',
+#        'HOST':'monorail.proxy.rlwy.net',
+#        'PORT':'37944',
+#    }
+#}
 
 # Password validation, Internationalization, Static files, etc. continue...
 
@@ -84,15 +86,12 @@ DATABASES = {
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gastrosal.settings')
 application = get_wsgi_application()
 
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'jobdata/static'),
 ]
-
 
 LOGGING = {
     'version': 1,
